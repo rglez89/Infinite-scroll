431cfe63355d3d59044996fefe19f16011021c10
Rudy <codingafter30@gmail.com>
2022-06-19 03:33:53 -0400

    Addding imageLoaded function

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = '8jP3xQitHC3uClSdvWSwyrZVguDmUf_Q1L1pVTz1b7U';
 const apiUrl =`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded(){
+    console.log('Image loaded');
+    imagesLoaded++;
+    console.log(imagesLoaded);
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        console.log('ready =', ready);
+    }
+}
 
 // Helper function to set attributes on DOM elements
 function setAttributes(element, attributes){
@@ -19,6 +34,9 @@ function setAttributes(element, attributes){
 // Create elements for links & photos, add to DOM
 
 function diplayPhotos(){
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('total images', totalImages);
     // Run fuction for each object in photosArray
     photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
@@ -34,7 +52,8 @@ function diplayPhotos(){
         alt: photo.alt_description,
         title: photo.alt_description,
     });
-
+    // Event listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -48,7 +67,6 @@ async function getPhotos(){
         const response = await fetch(apiUrl);
         photosArray = await response.json();
         diplayPhotos();
-        console.log(photosArray);
 
     } catch (error){
         // Catch error here
@@ -57,7 +75,8 @@ async function getPhotos(){
 
 // Checking to see if scrolling near bottom of page, load more photos
 window.addEventListener('scroll', () =>{
-    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
         getPhotos();
     }
 });
